d473f7d44496db4e393c2e0c9bc2bca703d6b715
karwal04 <pushpinder.karwal@gmail.com>
2017-04-22 09:46:58 +0530

    test_change

--- app/src/main/java/com/example/bittu/githubtest/MainActivity.java
+++ app/src/main/java/com/example/bittu/githubtest/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.bittu.githubtest;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn1=(Button)findViewById(R.id.butn1);
     }
 }
